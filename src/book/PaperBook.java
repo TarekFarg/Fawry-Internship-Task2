dacfe6a736fea1d32e1288695e625f1bc6bf9046
TarekFarg <tm747632@gmail.com>
2025-07-08 19:34:56 +0300

    Update PaperBook.java

--- src/book/PaperBook.java
+++ src/book/PaperBook.java
@@ -1,4 +1,27 @@
 package book;
 
+import service.ShippingService;
+
 public class PaperBook extends Book{
+
+    private int stock ;
+    public PaperBook(String ISBN , String title , int publicationYear , double price , int stock)
+    {
+        super(ISBN,title,publicationYear,price);
+        this.stock = stock ;
+    }
+
+    @Override
+    public void buy(int quantity, String email, String address) {
+        if(quantity > stock)
+        {
+            // do something
+            return;
+        }
+        ShippingService.ShipTo(address);
+        System.out.println("Paid amount: " + quantity*price);
+        stock -= quantity ;
+    }
+
+
 }
