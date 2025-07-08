92c2953245a83c5043b257478dd32461ef5b2c8a
TarekFarg <tm747632@gmail.com>
2025-07-08 19:34:58 +0300

    Update ShowcaseBook.java

--- src/book/ShowcaseBook.java
+++ src/book/ShowcaseBook.java
@@ -1,4 +1,15 @@
 package book;
 
 public class ShowcaseBook extends Book {
+
+    public ShowcaseBook(String ISBN , String title , int publicationYear , double price)
+    {
+        super(ISBN,title,publicationYear,price) ;
+    }
+
+    @Override
+    public void buy(int quantity, String email, String address) {
+        // do something
+        return;
+    }
 }
