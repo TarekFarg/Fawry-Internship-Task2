1931d9a2307e3b521bd5f528cdfe68d4351910c9
TarekFarg <tm747632@gmail.com>
2025-07-08 19:34:55 +0300

    Update EBook.java

--- src/book/EBook.java
+++ src/book/EBook.java
@@ -1,4 +1,19 @@
 package book;
 
+import service.MailService;
+
 public class EBook extends Book{
+    private String fileType ;
+
+    public EBook(String ISBN , String title , int publicationYear , double price , String fileType)
+    {
+        super(ISBN, title, publicationYear, price);
+        this.fileType = fileType ;
+    }
+
+    @Override
+    public void buy(int quantity, String email, String address) {
+        MailService.sendTo(email);
+        System.out.println("Paid amount: " + price);
+    }
 }
