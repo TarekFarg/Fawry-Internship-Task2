dd1c9c8aedfab31bdf999b3646ab9df5bf4f831d
TarekFarg <tm747632@gmail.com>
2025-07-08 19:34:54 +0300

    Update Book.java

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,4 +1,32 @@
 package book;
 
 public abstract class Book {
+    protected String ISBN ;
+    protected String title ;
+    protected int publicationYear ;
+    protected double price ;
+
+    public Book(String ISBN , String title , int publicationYear , double price)
+    {
+        this.ISBN = ISBN ;
+        this.title = title ;
+        this.publicationYear = publicationYear ;
+        this. price = price ;
+    }
+
+    public String getISBN()
+    {
+        return ISBN ;
+    }
+
+    public String getTitle()
+    {
+        return title ;
+    }
+
+    public int getPublicationYear()
+    {
+        return publicationYear ;
+    }
+     public abstract void buy(int quantity , String email , String address) ;
 }
